056673ff3f60bea09e588b258003685ae49b8c75
McKathlin <mckathlin@gmail.com>
2024-11-25 16:53:37 +0000

    Implement rudimentary board functions

--- TicTacToe.js
+++ TicTacToe.js
@@ -5,32 +5,64 @@
 // Model
 //=============================================================================
 
-// GameBoard
+const TicTacToe = (function () {
+    const board = (function() {
+        const WIDTH = 3;
+        const HEIGHT = 3;
+        const cells = new Array(WIDTH * HEIGHT);
 
-// Players
+        const getAtIndex = function(index) {
+            return cells[index];
+        };
 
-// Game state tracking
+        const setAtIndex = function(index, mark) {
+            cells[index] = mark;
+        };
 
-//=============================================================================
-// View
-//=============================================================================
+        const log = function() {
+            let idx = 0;
+            for (let r = 0; r < HEIGHT; r++) {
+                let currentRow = new Array(WIDTH);
+                for (let c = 0; c < WIDTH; c++) {
+                    currentRow[c] = getAtIndex(idx) ?? "_";
+                    idx++;
+                }
+                console.log(currentRow.join(" "));
+            }
+            console.log("");
+        };
 
-// Refresh the board
+        return { getAtIndex, setAtIndex, log };
+    })();
 
-// Refresh a cell
+    // Players
 
-// Display game state
+    // Game state tracking
+
+    // Public returnables
+
+    return { board };
+})();
 
 //=============================================================================
 // Controller
 //=============================================================================
 
-// Mark Cell
+// Mark cell
 
-// Restart Game
+// Restart game
+
+// Refresh cell
+
+// Refresh board
 
 //=============================================================================
 // Init
 //=============================================================================
 
-console.log("The game is loaded!");
+TicTacToe.board.log();
+
+TicTacToe.board.setAtIndex(0, "X");
+TicTacToe.board.setAtIndex(7, "O");
+
+TicTacToe.board.log();
