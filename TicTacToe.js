dfd4936d63649765e2baf38e94237360460d1a9e
McKathlin <mckathlin@gmail.com>
2024-11-25 17:43:07 +0000

    Add diagonal win checking

--- TicTacToe.js
+++ TicTacToe.js
@@ -24,7 +24,7 @@ const TicTacToe = (function () {
         };
 
         const getAtCoords = function(col, row) {
-            return cells[_indexAtCoords(col, row)];
+            return cells[indexAtCoords(col, row)];
         };
 
         const isClearAtIndex = function(index) {
@@ -40,7 +40,7 @@ const TicTacToe = (function () {
         };
 
         const setAtCoords = function(col, row, mark) {
-            cells[_indexAtCoords(col, row)] = mark;
+            cells[indexAtCoords(col, row)] = mark;
         };
 
         const log = function() {
@@ -56,15 +56,17 @@ const TicTacToe = (function () {
             console.log("");
         };
 
-        const _indexAtCoords = function(col, row) {
+        const indexAtCoords = function(col, row) {
             return (row * WIDTH) + col;
         };
 
         clear();
         return {
+            WIDTH, HEIGHT,
             getAtCoords, getAtIndex,
             isClearAtCoords, isClearAtIndex,
             setAtCoords, setAtIndex,
+            indexAtCoords,
             log
         };
     })();
@@ -73,6 +75,7 @@ const TicTacToe = (function () {
 
     const createPlayer = function(symbol) {
         const name = symbol;
+        const mark = symbol;
 
         const markBoardIndex = function(board, index) {
             if (board.isClearAtIndex(index)) {
@@ -82,7 +85,7 @@ const TicTacToe = (function () {
             }
         };
 
-        return { name, markBoardIndex };
+        return { name, mark, markBoardIndex };
     };
 
     const _players = [
@@ -104,7 +107,68 @@ const TicTacToe = (function () {
 
     // Game win tracking
 
+    let _winner = null;
 
+    const winner = function() {
+        return _winner;
+    };
+
+    const checkWin = function(player) {
+        if (_getWinIndexes(player.mark)) {
+            _winner = player;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const _getWinIndexes = function(mark) {
+        return _getColumnWinIndexes(mark)
+            || _getRowWinIndexes(mark)
+            || _getDiagonalWinIndexes(mark);
+    };
+
+    const _getColumnWinIndexes = function(mark) {
+
+    };
+
+    const _getRowWinIndexes = function(mark) {
+
+    };
+
+    const _getDiagonalWinIndexes = function(mark) {
+        if (board.WIDTH != board.HEIGHT) {
+            return null;
+        }
+        const length = board.WIDTH;
+
+        // Top left to bottom right
+        let winIndexes = [];
+        for (let i = 0; i < length && winIndexes !== null; i++) {
+            if (board.getAtCoords(i, i) == mark) {
+                winIndexes += board.indexAtCoords(i, i);
+            } else {
+                winIndexes = null;
+            }
+        }
+
+        if (winIndexes) {
+            return winIndexes;
+        }
+
+        // Top right to bottom left
+        winIndexes = [];
+        for (let r = 0; r < length && winIndexes !== null; r++) {
+            let c = (length - 1) - r;
+            if (board.getAtCoords(c, r) == mark) {
+                winIndexes += board.indexAtCoords(c, r);
+            } else {
+                winIndexes = null;
+            }
+        }
+
+        return winIndexes;
+    };
 
     // Game actions
 
@@ -112,7 +176,7 @@ const TicTacToe = (function () {
 
     // Public returnables
 
-    return { board, currentPlayer };
+    return { board, currentPlayer, winner, checkWin };
 })();
 
 //=============================================================================
@@ -131,5 +195,8 @@ const TicTacToe = (function () {
 // Init
 //=============================================================================
 
-TicTacToe.currentPlayer().markBoardIndex(TicTacToe.board, 3);
+TicTacToe.currentPlayer().markBoardIndex(TicTacToe.board, 0);
+TicTacToe.currentPlayer().markBoardIndex(TicTacToe.board, 8);
+TicTacToe.currentPlayer().markBoardIndex(TicTacToe.board, 4);
 TicTacToe.board.log();
+console.log(TicTacToe.checkWin(TicTacToe.currentPlayer()));
